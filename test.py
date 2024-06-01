dc9d083031fb5428b5c194d600bbcbd1db19cef1
Bjørn Funch Schrøder <Bjornfschroder@gmail.com>
2024-06-01 23:28:41 +0200

    rm test

--- test.py
+++
@@ -1,7 +0,0 @@
-
-from SIFpy import sif2array
-import os
-
-file = os.path.abspath('/Users/bjornfunchschroder/Desktop/Bjørn/WSe2Mono_SHG_150lw_1point7mW_20s.sif')
-data = sif2array(target=file, reduce_noise=False, window='narrow').get_data()
-print(data)
